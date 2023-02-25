293290f834649008879a09eeb9fa3cd0e94fc289
JiteshMurarka <116188595+JiteshMurarka@users.noreply.github.com>
2023-02-26 00:26:45 +0530

    first commit

---
+++ posts/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name= 'index'),
+    path('post/<str:pk>', views.post, name = 'post')
+]
+
+'''from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('post/<str:pk>', views.post, name='post')
+]'''
